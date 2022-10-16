42aafea34c657ee21e39b5b0104c617f7bda0dff
sayoung0820 <115957814+sayoung0820@users.noreply.github.com>
2022-10-16 19:42:23 -0400

    Add files via upload

---
+++ treasurehunt/practice.js
@@ -0,0 +1,19 @@
+var yourName = prompt("Please enter your name")
+
+var adjective = prompt("Please enter an adjective.")
+var noun1 = prompt("Please enter a noun.")
+var liquid = prompt("Please enter a liquid.")
+var bodyPart = prompt("Please enter a bodypart.")
+var verb = prompt("Please enter a verb.")
+var noun = prompt("Please enter a noun.")
+var place = prompt("Please enter a place.")
+
+
+var story = alert(`Once Upon a time there were some intrepid explorers who wandered into the amazon. They found some amazing things! Piranhas are more ${adjective} during the day, so make sure you cross the ${noun} at night. Piranhas are attracted to fresh ${liquid} and will most likely take a bite out of your ${bodypart} if you ${verb}. Whatever you do, if you have an open ${noun} try and find another way to get back to ${place}. Good luck!`)
+
+const worlddomination = () => {
+var name = prompt( "Hello what is your name")
+var code = prompt ("Do you know how to code?")
+if(code.toLowerCase() === `yes`) {alert (`You will rule the world, ${name}`)}
+else {alert ("Well, good luck with that.")}
+}
